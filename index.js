357c7bd18e0dc9f792974602445dbd19c9cc8370
Ivan.Biletskyi.Sisense <ivan.biletskyi@sisense.com>
2019-06-10 01:08:19 +0300

    ball in the box completed

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
+var DEFAULT_SPEED = 1;
+var BALL_DIAMETER = 20;
+var boardSize = { x: 400, y: 300 };
 var ball = document.getElementById("ball");
-setInterval(gameLoop, 20);
+var currentDirection = {
+    x: 1,
+    y: 1
+}
+setInterval(gameLoop, 1);
 
 function gameLoop() {
-    var oldBallX = parseInt(ball.style.left || 0);
-    var oldBallY = parseInt(ball.style.top || 0);
+    var ballPosition = {
+        x: parseInt(ball.style.left || 0),
+        y: parseInt(ball.style.top || 0)
+    }
 
-    var newBallX = oldBallX + 5;
-    var newBallY = oldBallY + 5;
-    ball.style.left = `${newBallX}px`;
-    ball.style.top = `${newBallY}px`;
+    var newBallPosition = getNewBallPosition(ballPosition);
+    ball.style.left = `${newBallPosition.x}px`;
+    ball.style.top = `${newBallPosition.y}px`;
 }
 
-function getNewBallPosition(oldBallX, oldBallY) {
+function getNewBallPosition(oldBallPosition) {
+    var newBallPosition = {
+        x: oldBallPosition.x + DEFAULT_SPEED * currentDirection.x,
+        y: oldBallPosition.y + DEFAULT_SPEED * currentDirection.y
+    }
 
+    var intersections = getIntersections(newBallPosition);
+    if (Object.values(intersections).includes(true)) {
+        if (intersections.top || intersections.bottom) {
+            currentDirection.y = -(currentDirection.y);
+        }
+        if (intersections.left || intersections.right) {
+            currentDirection.x = -(currentDirection.x);
+        }
+    }
+
+    return newBallPosition;
 }
+
+function getIntersections(ballPosition) {
+    var ballSizes = {
+        xStart: ballPosition.x,
+        xEnd: ballPosition.x + BALL_DIAMETER,
+        yStart: ballPosition.y,
+        yEnd: ballPosition.y + BALL_DIAMETER
+    };
+    var intersections = {
+        top: ballSizes.yStart < 0,
+        left: ballSizes.xStart < 0,
+        bottom: ballSizes.yEnd > boardSize.y,
+        right: ballSizes.xEnd > boardSize.x
+    }
+    return intersections;
+};
